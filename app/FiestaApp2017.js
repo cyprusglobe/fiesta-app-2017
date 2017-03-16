8c30cbd1c6161ee0b8f8341b20b24d776ad012d6
Kelvin Jones <pcmagic5000@gmail.com>
2017-03-16 13:42:02 -0600

    remove random text
    Signed-off-by: Kelvin Jones <pcmagic5000@gmail.com>

--- app/FiestaApp2017.js
+++ app/FiestaApp2017.js
@@ -24,7 +24,6 @@ class FiestaApp2017 extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
-        <Text>Add random text to test circleci</Text>
       </View>
     )
   }
