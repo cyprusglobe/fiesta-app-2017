18f7d2005622cd6efa27adb1e60076b3b50b803f
Kelvin Jones <pcmagic5000@gmail.com>
2017-03-17 00:10:19 -0600

    Lets try this one more time

--- android/app/src/main/java/com/fiestaapp2017/MainApplication.java
+++ android/app/src/main/java/com/fiestaapp2017/MainApplication.java
@@ -3,9 +3,6 @@ package com.fiestaapp2017;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.codepush.react.CodePush;
-import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
-import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -17,12 +14,6 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-
-    @Override
-    protected String getJSBundleFile() {
-      return CodePush.getJSBundleFile();
-    }
-
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -31,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
-            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
-            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics))
+          new MainReactPackage()
       );
     }
   };
--- ios/Pods/Target Support Files/Pods-FiestaApp2017-tvOS/Pods-FiestaApp2017-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FiestaApp2017_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_FiestaApp2017_tvOS
-@end
