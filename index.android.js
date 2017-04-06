5b5d80dbc0e786012f5902019e4f1f20c79345f2
Kelvin Jones <pcmagic5000@gmail.com>
2017-04-06 12:08:06 -0600

    cleanup unused code.

--- index.android.js
+++ index.android.js
@@ -25,26 +25,6 @@ function registerScreens() {
 
 registerScreens();
 
-// start the app
-// Navigation.startSingleScreenApp({
-//   screen: {
-//     screen: 'example.Home', // unique ID registered with Navigation.registerScreen
-//     title: 'Home', // title of the screen as appears in the nav bar (optional)
-//     leftButtons: [
-// 			{
-// 				id: 'sideMenu'
-// 			}
-// 		],
-//     navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
-//     navigatorButtons: {} // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
-//   },
-//   drawer: {
-//     left: {
-//       screen: 'example.Menu',
-//     }
-//   }
-// });
-
 syncEnchancer(Navigation.startSingleScreenApp({
   screen: {
     screen: 'example.Home', // unique ID registered with Navigation.registerScreen
@@ -63,20 +43,3 @@ syncEnchancer(Navigation.startSingleScreenApp({
     }
   }
 }));
-
-// syncEnchancer(Navigation.startTabBasedApp({
-//   tabs: [
-//     {
-//       label: 'Home',
-//       screen: 'example.Home', // this is a registered name for a screen
-//       icon: require('./images/one.png'),
-//       // selectedIcon: require('../img/one_selected.png'), // iOS only
-//       title: 'Home'
-//     }
-//   ],
-//   drawer: {
-//     left: {
-//       screen: 'example.Menu',
-//     }
-//   }
-// }));
